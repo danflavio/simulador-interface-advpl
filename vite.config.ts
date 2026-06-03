b8eb66115171ad608dff514c4395a127c892166b
Daniel Flávio <danflavio@gmail.com>
2026-06-03 15:41:36 -0300

    fix: corrige referência de branch para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/simulador-interface-advpl/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
